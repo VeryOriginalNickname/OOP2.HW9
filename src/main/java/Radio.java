b4a310613cf870b2ceb3138406441f019ca648cc
Dmitry Kurganov <dimacup30@gmail.com>
2023-07-13 20:27:57 +0300

    Reformat

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,30 +2,36 @@ public class Radio {
     public int currentRadioStationNumber;
     public int currentVolume;
 
-    public void setRadioStationNext(){
-        if (currentRadioStationNumber==9)
-        {
+    public void setRadioStationNext() {
+        if (currentRadioStationNumber == 9) {
             currentRadioStationNumber = 0;
+        } else {
+            currentRadioStationNumber = currentRadioStationNumber + 1;
         }
-        else {currentRadioStationNumber = currentRadioStationNumber+1;}
     }
 
-    public void setRadioStationPrev(){
-        if (currentRadioStationNumber==0)
-        {
+    public void setRadioStationPrev() {
+        if (currentRadioStationNumber == 0) {
             currentRadioStationNumber = 9;
+        } else {
+            currentRadioStationNumber = currentRadioStationNumber - 1;
         }
-        else {currentRadioStationNumber = currentRadioStationNumber-1;}
     }
 
-    public void setVolumeDown(){
-        if (currentVolume == 0){currentVolume = currentVolume;}
-        else{currentVolume = currentVolume - 1;}
+    public void setVolumeDown() {
+        if (currentVolume == 0) {
+            currentVolume = currentVolume;
+        } else {
+            currentVolume = currentVolume - 1;
+        }
     }
 
-    public void setVolumeUp(){
-        if (currentVolume == 100){currentVolume = currentVolume;}
-        else{currentVolume = currentVolume + 1;}
+    public void setVolumeUp() {
+        if (currentVolume == 100) {
+            currentVolume = currentVolume;
+        } else {
+            currentVolume = currentVolume + 1;
+        }
     }
 
 }
